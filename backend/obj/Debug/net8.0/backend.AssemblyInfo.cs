52f6e6a106f05c81698b68675871bd2237b122e6
Curtis Clement <curtis.clement@wonderkind.com>
2025-03-18 19:47:13 +0100

    add backend packages for database management

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5180d971b296c257f990765e4201174d8dc94e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3ab3ab8e49a7bec730f75b4dadb9c465c8e987")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
